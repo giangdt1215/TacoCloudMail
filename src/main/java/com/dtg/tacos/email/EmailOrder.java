190872e404fe5507370ca89ae2e93400cea4e5b1
GiangDT <giangdt1215@gmail.com>
2022-10-15 22:41:14 +0700

    Add EmailOrder entity

---
+++ src/main/java/com/dtg/tacos/email/EmailOrder.java
@@ -0,0 +1,17 @@
+package com.dtg.tacos.email;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class EmailOrder {
+
+    private final String email;
+    private List<Taco> tacos = new ArrayList<>();
+
+    public void addTaco(Taco taco){
+        tacos.add(taco);
+    }
+}
