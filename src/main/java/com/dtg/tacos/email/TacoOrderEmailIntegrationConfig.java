ee624df9904ff932a0c5354d252183b86544927c
GiangDT <giangdt1215@gmail.com>
2022-10-15 22:35:32 +0700

    Add EmailIntegrationConfig class

---
+++ src/main/java/com/dtg/tacos/email/TacoOrderEmailIntegrationConfig.java
@@ -0,0 +1,26 @@
+package com.dtg.tacos.email;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.dsl.Pollers;
+import org.springframework.integration.mail.dsl.Mail;
+
+@Configuration
+public class TacoOrderEmailIntegrationConfig {
+
+    @Bean
+    public IntegrationFlow tacoOrderEmailFlow(
+            EmailProperties emailProps,
+            EmailToOrderTransformer emailToOrderTransformer,
+            OrderSubmitMessageHandler orderSubmitMessageHandler){
+       return IntegrationFlows
+               .from(Mail.imapInboundAdapter(emailProps.getImapUrl()),
+                    e -> e.poller(
+                       Pollers.fixedDelay(emailProps.getPollRate())))
+               .transform(emailToOrderTransformer)
+               .handle(orderSubmitMessageHandler)
+               .get();
+    }
+}
