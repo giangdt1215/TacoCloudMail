053a6a204ff40e4717f7ac4613083e259d822c12
GiangDT <giangdt1215@gmail.com>
2022-10-15 22:21:45 +0700

    First commit SpringBoot based application

---
+++ src/main/java/com/dtg/tacos/TacoCloudMailApplication.java
@@ -0,0 +1,13 @@
+package com.dtg.tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudMailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dtg/tacos/TacoCloudMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dtg.tacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudMailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
