6dcfb61a2638ae75d9dac7d69cebc850cedba1b1
Dang Giang <gianggiang1512@gmail.com>
2022-10-26 20:44:49 +0700

    Add gmx config, add java mail properties

--- src/main/java/com/dtg/tacos/email/TacoOrderEmailIntegrationConfig.java
+++ src/main/java/com/dtg/tacos/email/TacoOrderEmailIntegrationConfig.java
@@ -6,6 +6,9 @@ import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.dsl.Pollers;
 import org.springframework.integration.mail.dsl.Mail;
+import org.springframework.integration.support.PropertiesBuilder;
+
+import java.util.function.Consumer;
 
 @Configuration
 public class TacoOrderEmailIntegrationConfig {
@@ -16,7 +19,15 @@ public class TacoOrderEmailIntegrationConfig {
             EmailToOrderTransformer emailToOrderTransformer,
             OrderSubmitMessageHandler orderSubmitMessageHandler){
        return IntegrationFlows
-               .from(Mail.imapInboundAdapter(emailProps.getImapUrl()),
+               .from(Mail.imapInboundAdapter(emailProps.getImapUrl())
+                       .shouldMarkMessagesAsRead(true)
+                       .shouldDeleteMessages(false)
+                       .maxFetchSize(10)
+                       .javaMailProperties
+                               (p -> p.put("mail.imap.socketFactory.class", "javax.net.ssl.SSLSocketFactory")
+                                       .put("mail.imap.socketFactory.fallback", false)
+                                       .put("mail.store.protocol", "imaps")
+                                       .put("mail.debug", true)),
                     e -> e.poller(
                        Pollers.fixedDelay(emailProps.getPollRate())))
                .transform(emailToOrderTransformer)
